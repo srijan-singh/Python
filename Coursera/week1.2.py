5e1b39b9dd54fb7a8e47a4dfdec91353b74dd754
Srijan Singh <srijanssj5goku@gmail.com>
2021-05-24 13:42:13 +0530

    initial

--- Coursera/week1.2.py
+++
@@ -1,9 +0,0 @@
-max = int(input())
-elements = input()
-elements = elements.split()
-list = []
-for i in elements:
-    list.append(int(i))
-list.sort()
-result = list[-1] * list[-2]
-print(result)
---
+++ Maths/Newton_Rhapson.py
@@ -0,0 +1,11 @@
+import math
+
+x_0 = (x0+x1)/2
+x = 0.6823
+
+
+f1 = x*x*x +x -1
+f1_ = 3*x*x + 1
+result = x - (f1)/(f1_)
+
+print(result)
