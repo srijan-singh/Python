f3313715e72d4fe60a28a41c0d0492306d96bfcc
Srijan Singh <srijanssj5goku@gmail.com>
2022-04-17 22:43:22 +0530

    RPC

---
+++ Distributed/RPC/client.py
@@ -0,0 +1,7 @@
+#client.py
+
+import xmlrpc.client
+
+with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
+    print("3 is even: %s" % str(proxy.is_even(3)))
+    print("100 is even: %s" % str(proxy.is_even(100)))
